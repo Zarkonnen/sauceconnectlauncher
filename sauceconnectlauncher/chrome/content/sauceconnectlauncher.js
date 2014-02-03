31d202928d21ab01b544894081332ba92ccfe187
Zarkonnen <david.stark@zarkonnen.com>
2014-02-03 13:16:10 +0100

    Towards a functioning setup.

--- sauceconnectlauncher/chrome/content/sauceconnectlauncher.js
+++ sauceconnectlauncher/chrome/content/sauceconnectlauncher.js
@@ -1,12 +1,68 @@
 var sauceConnectLauncher = {};
 
 var active = false;
+var proc = null;
+var obs = null;
 
 sauceConnectLauncher.run = function() {
   if (!active) { start(); } else { stop(); }
 };
 
+var myAddon = null;
+
+Components.utils.import("resource://gre/modules/AddonManager.jsm");
+AddonManager.getAddonByID("sauceconnectlauncher@saucelabs.com", function(addon) {
+  myAddon = addon;
+});
+
 function start() {
+  startExecutable();
+  uiShowRunning();
+  active = true;
+}
+
+function stop() {
+  stopExecutable();
+  //uiShowNotRunning();
+  //active = false;
+}
+
+function getExecutableFile() {
+  return myAddon.getResourceURI("/chrome/content/run.sh").QueryInterface(Components.interfaces.nsIFileURL).file;
+  /*var f = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+  f.initWithPath("/Applications/Chess.app/Contents/MacOS/Chess");
+  return f;*/
+}
+
+function getExecutableFile2() {
+  return myAddon.getResourceURI("/chrome/content/Sauce-Connect.jar").QueryInterface(Components.interfaces.nsIFileURL).file;
+}
+
+function startExecutable() {
+  obs = {
+    observe: function(a, b, c) {
+      alert(a + " / " + b + " / " + c);
+      uiShowNotRunning();
+      proc = null;
+      obs = null;
+      active = false;
+    }
+  };
+  var sh = Components.classes["@mozilla.org/file/local;1"].createInstance(Components.interfaces.nsILocalFile);
+  sh.initWithPath("/bin/sh");
+  proc = Components.classes["@mozilla.org/process/util;1"].createInstance(Components.interfaces.nsIProcess);
+  proc.init(sh);
+  proc.runAsync([getExecutableFile().path, getExecutableFile2().path, prompt("Username?"), prompt("Access key?")], 4, obs);
+}
+
+function stopExecutable() {
+  if (proc != null) {
+    alert("Stopping!");
+    proc.kill();
+  }
+}
+
+function uiShowRunning() {
   document.getElementById('sauceConnectLauncher-status-bar-icon').src = "chrome://sauceconnectlauncher/skin/status-bar.png";
   document.getElementById('sauceConnectLauncher-status-bar-icon').setAttribute('tooltiptext', _('deactivatesauceconnect'));
   var el = document.getElementById('sauceConnectLauncher-menuitem');
@@ -18,11 +74,9 @@ function start() {
     el.setAttribute('tooltiptext', _('deactivatesauceconnect'));
     el.className = "toolbar-on";
   }
-  
-  active = true;
 }
 
-function stop() {
+function uiShowNotRunning() {
   document.getElementById('sauceConnectLauncher-status-bar-icon').src = "chrome://sauceconnectlauncher/skin/status-bar-off.png";
   document.getElementById('sauceConnectLauncher-status-bar-icon').setAttribute('tooltiptext', _('activatesauceconnect'));
   var el = document.getElementById('sauceConnectLauncher-menuitem');
@@ -34,8 +88,6 @@ function stop() {
     el.setAttribute('tooltiptext', _('activatesauceconnect'));
     el.className = "toolbar-off";
   }
-  
-  active = false;
 }
 
 function _(name) {
